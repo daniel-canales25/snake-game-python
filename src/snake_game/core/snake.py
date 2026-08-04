23fbbe678beadad9db28ae0794ae69c4c1aebb54
daniel-canales25 <danielcanalestaylor@hotmail.com>
2026-08-04 16:41:08 -0500

    comentatio_logica_cabeza_snake

--- src/snake_game/core/snake.py
+++ src/snake_game/core/snake.py
@@ -13,7 +13,7 @@ class Snake:
         self.direction = self.nextDirection
         #Desempaquetado de tuplas de body x , y
         headX, headY = self.body[0]
-        #Al moverse 1 cuadro la cabeza ahora cambia
+        #Al moverse 1 cuadro la cabeza ahora cambia de lugar
         newHead = (headX + self.direction[0], headY + self.direction[1])
         self.body.insert(0, newHead)
         if not self.isGrowing:
